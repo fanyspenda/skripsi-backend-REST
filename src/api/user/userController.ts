2ad57868449ba4c913f5e4003dfd9aeecbdfefd9
fanyspenda <fanyervansyah.9c@gmail.com>
2020-05-22 11:29:50 +0700

    feat(user): list, update, delete, reset pass user

--- src/api/user/userController.ts
+++ src/api/user/userController.ts
@@ -1,3 +1,4 @@
+import paginate from "express-paginate";
 import jwt from "jsonwebtoken";
 import userModel from "./userModel";
 import { Request, Response } from "express";
@@ -7,24 +8,26 @@ interface authReq extends Request {
 }
 
 const userController = {
-	register: (req: Request, res: Response) => {
-		const data = {
-			name: req.body.name,
-			email: req.body.email,
-			level: req.body.level,
-			password: bcrypt.hashSync(req.body.password, 10),
-		};
+	register: async (req: Request, res: Response) => {
+		try {
+			const data = {
+				name: req.body.name,
+				email: req.body.email,
+				level: req.body.level,
+				password: bcrypt.hashSync(req.body.password, 10),
+			};
 
-		userModel
-			.create(data)
-			.then((doc) => {
-				res.status(200);
-				res.send(doc);
-			})
-			.catch((err) => {
-				res.status(401);
-				res.send(err);
-			});
+			const result = await userModel.findOne({ email: data.email });
+			if (result) {
+				return res.status(401).send("Email sudah terdaftar!");
+			}
+
+			const createResult = await userModel.create(data);
+			res.status(200);
+			res.send(createResult);
+		} catch (err) {
+			res.send(err);
+		}
 	},
 	login: (req: Request, res: Response) => {
 		userModel
@@ -49,6 +52,89 @@ const userController = {
 				res.status(401).send({ error: "user not found" });
 				res.end();
 			});
+	},
+	showAll: async (req: Request, res: Response) => {
+		const searchedname = req.query.name || "";
+		try {
+			const documentUser = await userModel
+				.find(
+					{
+						name: {
+							$regex: searchedname,
+							$options: "i",
+						},
+					},
+					{
+						name: true,
+						email: true,
+						level: true,
+					}
+				)
+				.limit(req.query.limit * 1)
+				.skip(req.skip);
+
+			const countUsers = await userModel.countDocuments({
+				name: {
+					$regex: searchedname,
+					$options: "i",
+				},
+			});
+			let pageCount = Math.ceil(countUsers / (req.query.limit * 1));
+			if (pageCount <= 0) pageCount = 1;
+			res.send({
+				has_more: paginate.hasNextPages(req)(pageCount),
+				pageCount,
+				countUsers,
+				pages: paginate.getArrayPages(req)(
+					4,
+					pageCount,
+					req.query.page * 1
+				),
+				data: documentUser,
+			});
+		} catch (error) {
+			return res.send(`error happened: ${error}`);
+		}
+	},
+	updateOne: async (req: Request, res: Response) => {
+		try {
+			const { name, email, level } = req.body;
+			if (!name || !email || !level)
+				return res.send("data not fullfiled");
+			const result = await userModel.findByIdAndUpdate(
+				req.params.id,
+				{
+					name: req.body.name,
+					email: req.body.email,
+					level: req.body.level,
+				},
+				{ new: true, fields: { name: true, email: true } }
+			);
+			return res.send(result);
+		} catch (error) {
+			return res.send(`error: ${error}`);
+		}
+	},
+	resetPass: async (req: Request, res: Response) => {
+		if (!req.body.password)
+			return res.send("data to reset password is not valid");
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		try {
+			await userModel.findByIdAndUpdate(req.params.id, {
+				password: hashedPassword,
+			});
+			res.send(200);
+		} catch (error) {
+			res.send(`error: ${error}`);
+		}
+	},
+	deleteUser: async (req: Request, res: Response) => {
+		try {
+			await userModel.findByIdAndDelete(req.params.id);
+			res.send(200);
+		} catch (error) {
+			res.send(`error: ${error}`);
+		}
 	},
 };
 
--- src/api/user/userRouter.ts
+++ src/api/user/userRouter.ts
@@ -4,5 +4,9 @@ const userRouter = Router();
 
 userRouter.post("/register", userController.register);
 userRouter.post("/login", userController.login);
+userRouter.get("/", userController.showAll);
+userRouter.put("/:id", userController.updateOne);
+userRouter.put("/resetPass/:id", userController.resetPass);
+userRouter.delete("/:id", userController.deleteUser);
 
 export default userRouter;
