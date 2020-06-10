7eaafb484290d4c5105ab86dcabfc945941e7ec0
fanyspenda <fanyervansyah.9c@gmail.com>
2020-06-10 12:43:44 +0700

    fix(add-major)

--- src/api/major/majorController.ts
+++ src/api/major/majorController.ts
@@ -43,7 +43,7 @@ export const majorController = {
 			const result = await majorModel.create({ ...req.body });
 			res.status(200).send(result);
 		} catch (error) {
-			res.status(500).send(error);
+			res.send(error);
 		}
 	},
 	updateOne: async (req: Request, res: Response) => {
